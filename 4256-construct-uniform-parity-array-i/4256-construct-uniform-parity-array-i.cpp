4f2529e0a935fc82cb7191e872219d17cca64fa6
Ashwin Rajhans <ashwinreaper10@gmail.com>
2026-09-02 20:00:42 +0530

    4256-CONSTRUCT-UNIFORM-PARITY-ARRAY-I Stats: Time: 0 ms (100%), Space: 30.4 MB (7.23%) - LeetHub

---
+++ 4256-construct-uniform-parity-array-i/4256-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // bool isOddd(vector<int>& nums){
+    //     for(int &i : nums){
+    //         if(i % 2) != 0{
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool uniformArray(vector<int>& nums1) {
+        //bool isOd = false;
+        return true;
+
+    }
+};
